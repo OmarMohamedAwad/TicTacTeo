7defc507442d0aeefbffeab011ee0376875371fb
OmarMohamedAwad <omar.awad825@gmail.com>
2021-02-17 00:27:49 +0200

    ReAdd Player Dashboard

--- TicTacTeo/src/tictacteo/TicTacTeo.java
+++ TicTacTeo/src/tictacteo/TicTacTeo.java
@@ -1,12 +1,36 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tictacteo;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class TicTacTeo {
+/**
+ *
+ * @author Omar Awad
+ */
+public class TicTacTeo extends Application{
 
-   
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MyDashboardGUI root = new MyDashboardGUI(primaryStage);
+        
+        Scene scene = new Scene(root, 600, 500);
+        
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
 }
